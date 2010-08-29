1aeae43ee9aefd65703cda287eef7b7e57e11c13
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-29 13:27:13 -0700

    shovels AVL records into a database

--- SddArchiver.java
+++ SddArchiver.java
@@ -3,11 +3,18 @@ import java.io.IOException;
 import its.backbone.sdd.*;
 import its.SQL.*;
 import java.util.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 class SddArchiver extends SddReceiver{
 
-  public SddArchiver() throws IOException {
+  Connection conn;
+
+  public SddArchiver( Connection conn ) throws IOException {
     super( "sdd.its.washington.edu", 8412 );
+    this.conn = conn;
     start();
   }
 
@@ -16,15 +23,26 @@ class SddArchiver extends SddReceiver{
   }
   
   public void extractedDataReceived(Hashtable ht, String serialNum) {
-    print((ContentsData)ht.get("AVL_DATA"));
+    ContentsData locations = (ContentsData)ht.get("AVL_DATA");
+    try{
+      insert(locations);
+    } catch( SQLException e ) {
+      System.out.println( "Failed to archive record" );
+      System.out.println( e );
+    }
   }
 
-  void print(ContentsData cd) {
+  void insert(ContentsData cd) throws SQLException{
     StringBuffer sb = new StringBuffer();
     sb.append(cd.getColumnNames()).append("\r\n");
     cd.resetRowIndex();
     while (cd.next()) {
-      sb.append(cd.getRowString(true,true));
+      String insertString = "INSERT INTO avl_data VALUES ("+cd.getRowString(true,true)+")";
+
+      Statement stmt = conn.createStatement();
+      int rs = stmt.executeUpdate(insertString);
+
+      sb.append(insertString);
       sb.append("\r\n");
     }		
     System.out.print(sb.toString());
--- main.java
+++ main.java
@@ -1,10 +1,27 @@
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 class Main {
 
   public static void main(String args[]) {
+      
+    Connection c = null;
+	  
     try {
-      SddArchiver sddArchiver = new SddArchiver();
+      // The second and third arguments are the username and password,
+      // respectively. They should be whatever is necessary to connect
+      // to the database.
+      c = DriverManager.getConnection("jdbc:postgresql://localhost:5433/brandon");
+    } catch (SQLException se) {
+      System.out.println("Couldn't connect: print out a stack trace and exit.");
+      se.printStackTrace();
+      System.exit(1);
+    }
+
+    try {
+      SddArchiver sddArchiver = new SddArchiver(c);
       System.out.println( sddArchiver );
     } catch( IOException e ) {
       System.out.println( e );
